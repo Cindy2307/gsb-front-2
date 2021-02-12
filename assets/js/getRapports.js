6881dfef194cde26aecdd01fc1b1e1f50d58d8f9
Cindy <cindy.ol@hotmail.fr>
2021-02-12 20:57:49 +0100

    modif

---
+++ assets/js/getRapports.js
@@ -0,0 +1,42 @@
+const rapports = document.querySelector("#rapports");
+
+const getRapports = () => {
+    const url = 'http://localhost:3000/gsb/rapport';
+
+    fetch(url)
+    .then(response => response.json())
+        .then((data) => {
+        for (const rapport of data) {
+            const date = new Date(rapport.date);
+            
+            rapports.insertAdjacentHTML('beforeEnd',
+                `
+                    <li>
+                        <div class="card rapport${rapport.id}">
+                            <div class="card-body">
+                                <div class="infos infos${rapport.id}">
+                                    <span>${date.getDate()}/${date.getMonth()}/${date.getFullYear()}</span>
+                                    <span>${rapport.bilan}</span>
+                                    <span>${rapport.motif}</span>
+                                </div>
+                                <div class="boutons">    
+                                    <button class="modifier modifierRapport${rapport.id}"><i class="fas fa-pen"></i></button>
+                                    <button class="supprimer supprimerRapport${rapport.id}"><i class="fas fa-trash-alt"></i></button>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                `
+            );
+
+            if (data.indexOf(rapport) < data.length - 1) {
+                document.querySelector(`.rapport${rapport.id}`).style.marginBottom = "0.3%";
+            }
+        }
+    })
+    .catch((error) => {
+        console.log(`Voici mon erreur ${error}`);
+    });
+}
+
+getRapports();
