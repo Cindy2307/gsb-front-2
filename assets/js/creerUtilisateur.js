661b6163fa3e89c89137debcf8158a28aeae5667
chrisLaf388 <christophe.lafenetre@gmail.com>
2021-03-01 20:06:58 +0100

    creer visiteur ok

---
+++ assets/js/creerUtilisateur.js
@@ -0,0 +1,42 @@
+const nom = document.querySelector("#nom");
+const creer = document.querySelector("#creer");
+const form = document.querySelector("form");
+const logo = document.querySelector("#logo");
+console.log(form)
+logo.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.location.href = "../html/utilisateursListe.html";
+});
+
+async function createUtilisateur() {
+    const url = `http://localhost:3000/gsb/visiteur`;
+    let response = "";
+    const formData = new FormData(form);
+    let object = {};
+
+    formData.forEach(function(value, key){
+        object[key] = value;
+    });
+
+    const responseJson = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(object)
+    })
+        .catch((error) => {
+            console.log(`Voici mon erreur ${error}`);
+        });
+
+    if (responseJson.status === 200) {
+        response = await responseJson.json();
+        alert("Le visiteur a bien été créé.");
+        document.location.href = "../html/utilisateursListe.html";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    createUtilisateur();
+});
