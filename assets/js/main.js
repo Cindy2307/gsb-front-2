3b0246d76adbc3acf2de958292eeb163ab7f270c
chris <christophe.lafenetre@gmail.com>
2021-04-04 20:04:54 +0200

    cors ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,42 @@
 const connexion = document.querySelector("#connexion");
+// connexion.addEventListener("click", (e) => {
+//     e.preventDefault();
+    
+// });
+// var myHeaders = new Headers();
+// myHeaders.append("Authorization", "Basic Q29jbzQ6Y291Y291NA==");
+// myHeaders.append("Content-Type", "application/json");
+// myHeaders.append("Cookie", "JSESSIONID=8DE2D6B332466F5F537E6C0802649E12");
+
+// var requestOptions = {
+//     method: 'GET',
+//     headers: myHeaders,
+//     body: raw,
+//     redirect: 'follow'
+//   };
+  
+//   fetch("localhost:3002/gsb/visiteur", requestOptions)
+//     .then(response => response.text())
+//     .then(result => console.log(result))
+//     .catch(error => console.log('error', error));
 
 async function login() {
+
     const login = document.querySelector("#login").value;
     const password = document.querySelector("#password").value;
+    
     const credential = window.btoa(login + ":" + password);
+    alert(login+password)
     console.log(credential);
     const url = "http://localhost:3002/gsb/login";
     let response = "";
 
     const responseJson = await fetch(url, {
+        
         headers: {
-            "test": "test",
             "Content-Type": "application/json",
-            "Authorization": "Basic Q29jbzpjb3Vjb3U="
+            "Authorization": "Basic " + credential
         },
-        mode: "no-cors",
     })
         .catch((error) => {
             console.log(`Voici mon erreur ${error}`);
